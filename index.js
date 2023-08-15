b8e697e9f671c89385cac56bec0593aee843bc96
toduyemi <temit96@hotmail.com>
2023-08-14 23:18:49 -0400

    add toggle erase feature still very buggy

--- index.js
+++ index.js
@@ -6,14 +6,36 @@ const gridContainer =  document.querySelector('.grid-container');
 const gridControl = document.querySelector('#grid-control');
 const gridControlValue = document.querySelector('.control-value');
 
+const shadeToggle = document.querySelector('#shade');
+const eraseToggle = document.querySelector('#eraser');
+
 let mouseDown = false;
 let gridPrint = false;
+let toggleErase = false;
 
 //control functionalities
 gridControl.addEventListener('input',  (e) => {
     const newSliderValue = e.target.value;
-    gridControlValue.textContent = `${newSliderValue}x${newSliderValue}`;
+    gridControlValue.textContent = `canvas: ${newSliderValue}x${newSliderValue}`;
 });
+
+
+
+eraseToggle.addEventListener('click', (e) => {
+    
+    toggleErase ^= true;
+    console.log(toggleErase);
+
+    // if (toggleErase){
+    //     eraseToggle.setAttribute('style', 'background-color: rgb(168, 168, 168);');
+    // }
+
+    // else {
+    //     eraseToggle.setAttribute('style', 'background-color: white;');
+    // }
+
+});
+
 
 //canvas functionalities
 function createGrid(dimension) {
@@ -60,7 +82,13 @@ gridControl.addEventListener('input', transformGrid);
 gridContainer.addEventListener('mousedown', (e) => {
     console.log('yo');
     mouseDown = true;
-    e.target.classList.add('paint');
+    if (!toggleErase) {
+        e.target.classList.add('paint')
+    }
+    else {
+        e.target.classList.add('erase');
+    }
+    
     
  });
 
@@ -69,11 +97,13 @@ gridContainer.addEventListener('mouseup', () => {
 });
 
 gridContainer.addEventListener('mouseover', (e) => {
-    console.log('yo');
     if (mouseDown) {
-        console.log('yo');
-        console.log(mouseDown);
-        e.target.classList.add('paint');
+        if (!toggleErase) {
+            e.target.classList.add('paint')
+        }
+        else {
+            e.target.classList.add('erase');
+        }
     }
     
 });
